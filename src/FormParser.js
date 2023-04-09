30398d73a8953ec54eb6da90d8667dd246d086c7
Ali <ali.shaikh@hotmail.com>
2023-04-09 16:00:34 -0700

    update 4/9/2023

--- src/FormParser.js
+++ src/FormParser.js
@@ -1,15 +1,17 @@
 // FormParser.js
-import { useParams } from 'react-router-dom';
-import { db } from "./Firebase-config"; 
+import { useParams } from "react-router-dom";
+import { db } from "./Firebase-config";
 import { collection, addDoc, doc, setDoc } from "firebase/firestore";
 
-
 export const FormParser = ({ onSelect }) => {
-  //const params = useParams();
-  //const collectionRef = collection(db, 'incidents', params.incidentId, 'forms');
-  const collectionRef = collection(db, 'incidents', '5dfc8TW4dAvTMdUWbGZV', 'forms');
-  console.log("look here:", collectionRef);
-//   onSelect('some item'); // call onSelect function
+  const params = useParams();
+  console.log("HERE", params);
+  // const collectionRef = collection(db, "incidents", params.incidentId, "forms");
+  // const collectionRef = collection(db, 'incidents', '5dfc8TW4dAvTMdUWbGZV', 'forms');
+  console.log("incidentid: ", params.incidentId);
+  console.log("formid: ", params.formId);
+  // console.log("look here:", collectionRef);
+  //   onSelect('some item'); // call onSelect function
 };
 
 export default FormParser;
